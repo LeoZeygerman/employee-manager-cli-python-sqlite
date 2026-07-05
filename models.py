0348ae7fda9a94f3d285a3e070e800d8425c7e35
BBYBOI <zeygermanleo@gmail.com>
2026-07-05 18:34:13 +0500

    add new func

--- models.py
+++ models.py
@@ -10,4 +10,13 @@ class Workers:
     def show_all(self):
         print(f'======\nID: {self.id} | Имя: {self.name} | Фамилия: {self.last_name}\nВозраст: {self.age}\nДолжность: {self.post} | Зарплата: {self.salary}\n=====')
         
-    
+
+class Transactions:
+    def __init__(self, name, type, amount, reason):
+        self.name = name
+        self.type = type
+        self.amount = amount
+        self.reason = reason
+        
+    def show_history(self):
+        print(f'=====\nИмя: {self.name}\nТип: {self.amount}\nРазмер: {self.amount}\nПричина: {self.reason}\n=====')
--- second_database.py
+++ second_database.py
@@ -1,4 +1,5 @@
 import sqlite3 as sq
+from models import Workers, Transactions
 
 DATABASE = 'fine_bonus.db'
 def connect():
@@ -11,12 +12,25 @@ def create_base_fine():
         
         cur.execute('''CREATE TABLE IF NOT EXISTS fine_bonus(
             staff_id INTEGER,
-            fine INTEGER,
-            bonus INTEGER)''')
+            type TEXT,
+            amount INTEGER NOT NULL DEFAULT 0,
+            reason TEXT)''')
         
-def add_fine(worker_id, fine):
+def add_fine_bonus(worker_id, type, amount, reason):
     with connect() as con:
         con.row_factory = sq.Row
         cur = con.cursor()
+        cur.execute('''INSERT INTO fine_bonus VALUES(?,?,?,?)'''(worker_id,type,amount,reason))
+        cur.execute('''SELECT staff.name, fine_bonus.type, fine_bonus.amount, fine_bonus.reason FROM fine_bonus JOIN staff ON fine_bonus.staff_id = staff.staff_id''')
         
-        cur.execute('''UPDATE fine_bonus SET fine = ? WHERE worker_id = ?''', (fine, worker_id))
+        
+        workers = []
+        for row in cur:
+            worker = Transactions(
+                row['name'],
+                row['type'],
+                row['amount'],
+                row['reason']
+            )
+            workers.append(worker)
+        return workers
